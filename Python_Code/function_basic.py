0a289929201385ab921cd98f15fc5386fd5aa325
Gunjan kumar sah <gunjankrsah802128@gmail.com>
2026-07-07 13:04:00 +0530

    Added File

---
+++ Python_Code/function_basic.py
@@ -0,0 +1,36 @@
+# def gunjan (a,b):
+#   sum = a+b
+#   print(sum)
+#   return sum
+
+# gunjan(2,5)
+# gunjan(34,67)
+# gunjan(2,4)
+
+
+
+def avrg(a,b,c):
+  sum = a+b+c
+  avg = sum / 3
+  print(avg)
+  return avg
+avrg(2,4,6)
+avrg(1,2,3)
+avrg(2,4,5)
+
+
+# def show(n):
+#   if(n == 0):
+#     return
+#   print(n)
+#   show(n-1)
+
+
+# def fact(n):
+#   if(n == 0 or n == 1):
+#     return 1
+#   else:
+#     return n*fact(n-1)
+#   print(fact(5))
+
+
