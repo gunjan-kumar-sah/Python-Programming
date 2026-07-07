8d5d2bb7b6a3e0f0a1fd80dd7da88960ab45db90
Gunjan kumar sah <gunjankrsah802128@gmail.com>
2026-07-07 12:02:06 +0530

    Add file

---
+++ Funny_Code/heart.py
@@ -0,0 +1,20 @@
+import turtle
+
+turtle.speed(1)
+turtle.bgcolor('black')
+turtle.pensize(3)
+def func():
+    for i in range(200):
+        turtle.right(1)
+        turtle.forward(1)
+turtle.color('red', 'pink')
+turtle.begin_fill()
+turtle.left(140)
+turtle.forward(111.65)
+func()
+turtle.left(120)
+func()
+turtle.forward(111.65)
+turtle.begin_fill()
+turtle.hideturtle()
+turtle.done()
